6211ccd65a893b27df40db4f18ee2aa9533fc80e
Vitaliy <gedion63@gmail.com>
2025-11-19 01:03:56 +0400

    создана функция get_date

--- src/widget.py
+++ src/widget.py
@@ -12,3 +12,7 @@ def mask_account_card(card_inf: str) -> str:
             card_inf[-16:], get_mask_card_number(card_inf[-16:])
         )  # Возвращает счет с подменой последних 16 значений
     """Создан модуль widget создана функция mask_account_card"""
+
+    def get_date(data_inf: str) -> str:
+        """Возвращает информацию о дате в формате число.месяц.год"""
+        return f"{data_inf[8:10]}.{data_inf[5:7]}.{data_inf[0:4]}"
